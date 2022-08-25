0c819876383d5775aa3fe651e231c9d90ec7776d
ltmartinelli <lucastrindademartinelli@gmail.com>
2022-08-25 19:37:38 -0300

    menu js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+    
+        
+    var menu = new Menu({
+        container: '.header-nav',
+        toggleBtn: '.header-btn-menu',
+        widthEnabled: 1024 
+    })
+    
+})()
